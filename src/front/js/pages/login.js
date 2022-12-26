7431fbc3b9d3482f0ee1786dc3f059d9133131ee
Juand30 <juandp2290@gmail.com>
2022-12-26 14:09:56 +0000

    act

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="E-Mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick}>Login</button>
+    </div>
+  );
+};
