155bb89339d9cbe285ef08d912cf3b6947490e80
danielo8417 <danieljuhe@gmail.com>
2023-01-02 09:50:50 +0000

    add auth0 package & index

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,12 +1,16 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include your index.scss file into the bundle
 import "../styles/index.css";
-
-//import your own components
 import Layout from "./layout";
+import { useAuth0 } from "@auth0/auth0-react";
 
-//render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-3stbnni4dkoomgjb.us.auth0.com"
+    clientId="04KUnCNLYelMOCKn1fMKme15bB0pEo1i"
+    redirectUri={window.location.origin}
+  >
+    <Layout />
+  </Auth0Provider>,
+  document.querySelector("#app")
+);
