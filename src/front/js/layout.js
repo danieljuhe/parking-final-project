419f8acfa8953a3077bb70979442c64e570bf8b1
agosfasanelli <agosfasanelli@gmail.com>
2023-01-08 13:19:12 +0000

    hasv

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import UserProfile from "./pages/userprofile";
+import CreateCar from "./pages/createCar";
 
 //create your first component
 const Layout = () => {
@@ -25,6 +26,7 @@ const Layout = () => {
             <Route element={<Home />} path="/" />
             <Route element={<Demo />} path="/demo" />
             <Route element={<Login />} path="/login" />
+            <Route element={<CreateCar />} path="/car" />
             <Route element={<UserProfile />} path="/privateuser" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
