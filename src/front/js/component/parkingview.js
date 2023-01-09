f4712aab1d9ee2f760df4a6e1f747c17f0346bc0
danielo8417 <danieljuhe@gmail.com>
2023-01-09 20:04:26 +0000

    merge juan

---
+++ src/front/js/component/parkingview.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const ParkingView = () => {
+  return (
+    <div className="main">
+      <div className="parkinglot">
+        <div className="col1">
+          <div className="c1p1"></div>
+          <div className="c1p2"></div>
+          <div className="c1p3"></div>
+          <div className="c1p4"></div>
+          <div className="c1p5"></div>
+          <div className="c1p6"></div>
+          <div className="c1p7"></div>
+          <div className="c1p8"></div>
+          <div className="c1p9"></div>
+          <div className="c1p10"></div>
+        </div>
+        <div className="col2">
+          <div className="c1p1"></div>
+          <div className="c1p2"></div>
+          <div className="c1p3"></div>
+          <div className="c1p4"></div>
+          <div className="c1p5"></div>
+          <div className="c1p6"></div>
+          <div className="c1p7"></div>
+          <div className="c1p8"></div>
+          <div className="c1p9"></div>
+          <div className="c1p10"></div>
+        </div>
+        <div className="col3">
+          <div className="c1p1"></div>
+          <div className="c1p2"></div>
+          <div className="c1p3"></div>
+          <div className="c1p4"></div>
+          <div className="c1p5"></div>
+          <div className="c1p6"></div>
+          <div className="c1p7"></div>
+          <div className="c1p8"></div>
+          <div className="c1p9"></div>
+          <div className="c1p10"></div>
+        </div>
+        <div className="col4">
+          <div className="c1p1"></div>
+          <div className="c1p2"></div>
+          <div className="c1p3"></div>
+          <div className="c1p4"></div>
+          <div className="c1p5"></div>
+          <div className="c1p6"></div>
+          <div className="c1p7"></div>
+          <div className="c1p8"></div>
+          <div className="c1p9"></div>
+          <div className="c1p10"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,13 +3,12 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { Login } from "./pages/login";
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
 import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import UserProfile from "./pages/userprofile";
 import { Reg } from "./pages/register";
-import Date_Time from "./pages/time";
+import Date_Time from "./component/time";
 
 const Layout = () => {
   const basename = process.env.BASENAME || "";
@@ -21,13 +20,9 @@ const Layout = () => {
           <Navbar />
           <Routes>
             <Route element={<Home />} path="/" />
-            <Route element={<Demo />} path="/demo" />
             <Route element={<Login />} path="/login" />
             <Route element={<Reg />} path="/register" />
-            <Route
-              element={<UserProfile logged={setLogged} />}
-              path="/privateuser"
-            />
+            <Route element={<UserProfile />} path="/privateuser" />
             <Route element={<Date_Time />} path="/date" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
