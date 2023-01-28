9c667c7cd2a7c4ee5145c9044cc050a9682e8277
danielo8417 <danieljuhe@gmail.com>
2023-01-28 08:34:56 +0000

    layaout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,11 +11,9 @@ import { Reg } from "./pages/register";
 import { ParkingView } from "./component/parkingview";
 import CreateCar from "./component/createCar";
 import CarsInfo from "./pages/carsInfo";
-import Date_Time from "./component/time";
 import Cars from "./pages/cars";
 import EditCar from "./pages/editCar";
 import { PriceGen } from "./component/pricegendata";
-import { Plazas } from "./component/pruebaparking";
 import AppPay from "./component/stripe";
 
 
@@ -39,7 +37,6 @@ const Layout = () => {
             <Route element={<CarsInfo />} path="/mycar" />
             <Route element={<EditCar />} path="/editcar/:car_id" />
             <Route element={<Cars />} path="/cars" />
-
             <Route element={<PriceGen />} path="/date" />
           </Routes>
 
