bef4245f3c8a22c8ec8bfc9a8d809968ded1d6f5
agosfasanelli <agosfasanelli@gmail.com>
2023-01-30 20:21:16 +0000

    funciona editar y eliminar ok

--- src/front/js/pages/cars.js
+++ src/front/js/pages/cars.js
@@ -20,6 +20,7 @@ const Cars = () => {
     navigate("/editcar/" + id);
   };
 
+
   const handleDeleteClick = (id) => {
     fetch(process.env.BACKEND_URL + "/api/delete_car/" + id, {
       method: "DELETE",
@@ -53,16 +54,37 @@ const Cars = () => {
               >
                 Editar
               </button>
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={() => handleDeleteClick(car.id)}
-              >
+              <button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                 Eliminar
               </button>
+              <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="staticBackdropLabel">WARNING</h5>
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                      Seguro que desea elimiar el vehiculo seleccionado?
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button"
+                        className="btn btn-secondary"
+                        data-bs-dismiss="modal">
+                        Cerrar</button>
+                      <button type="button"
+                        className="btn btn-danger"
+                        onClick={() => handleDeleteClick(car.id)}>
+                        Eliminar
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           );
         })}
+
       </div>
     </div>
   );
