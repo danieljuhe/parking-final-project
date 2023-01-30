e884825ad25e2df1caaaf77b045938df9ccc0fa8
Juand30 <juandp2290@gmail.com>
2023-01-30 19:41:19 +0000

    Arreglo layaout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -33,6 +33,7 @@ const Layout = () => {
             <Route element={<Reg />} path="/register" />
             <Route element={<UserProfile />} path="/privateuser" />
             <Route element={<AppPay />} path="/payment/:parking_id" />
+            <Route element={<PriceGen />} path="/date" />
             <Route element={<h1>Not found!</h1>} />
             <Route element={<CarsInfo />} path="/mycar" />
             <Route element={<EditCar />} path="/editcar/:car_id" />
