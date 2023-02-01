ba12caa86235b85b407f86ea3dc9a3cee8f2e7f3
danielo8417 <danieljuhe@gmail.com>
2023-02-01 19:18:23 +0000

    parkinssites

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,7 +5,8 @@ export const Home = () => {
   return (
     <div className="home">
       <div className="home2">
-        <div className="text-center mt-5">
+        <div className="side"></div>
+        <div className="center">
           <h1 className="title">Parking App</h1>
         </div>
       </div>
