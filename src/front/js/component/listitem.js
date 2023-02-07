3ca16ec2de6bfb11892ccc4ad0f842481a20bdd8
danielo8417 <danieljuhe@gmail.com>
2023-02-07 08:04:01 +0000

    dashboard

--- src/front/js/component/listitem.js
+++ src/front/js/component/listitem.js
@@ -3,7 +3,6 @@ import ListItemButton from '@mui/material/ListItemButton';
 import { emphasize, styled } from '@mui/material/styles';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PeopleIcon from '@mui/icons-material/People';
@@ -12,9 +11,38 @@ import LayersIcon from '@mui/icons-material/Layers';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Chip from '@mui/material/Chip';
 import HomeIcon from '@mui/icons-material/Home';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import DirectionsCarFilledOutlinedIcon from '@mui/icons-material/DirectionsCarFilledOutlined';
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Badge from '@mui/material/Badge';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        backgroundColor: '#44b700',
+        color: '#44b700',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
+    },
+    '@keyframes ripple': {
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
+    },
+}));
 
 const StyledBreadcrumb = styled(Chip)(({ theme }) => {
     const backgroundColor =
@@ -35,48 +63,64 @@ const StyledBreadcrumb = styled(Chip)(({ theme }) => {
 
 export const mainListItems = (
     <React.Fragment>
-        <Breadcrumbs aria-label="breadcrumb">
-            <StyledBreadcrumb
-                sx={{ fontSize: 14 }}
-                component="a"
-                icon={<HomeIcon sx={{ fontSize: 14 }} />}
-            /></Breadcrumbs><br />
+
+        <ListItemButton>
+            <ListItemIcon>
+                <Stack direction="row" spacing={2}>
+                    <StyledBadge
+                        overlap="circular"
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        variant="dot"
+                    >
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    </StyledBadge>
+                </Stack>
+            </ListItemIcon>
+        </ListItemButton>
+        <br />
+
         <ListItemButton>
             <ListItemIcon>
                 <DashboardIcon />
             </ListItemIcon>
             <ListItemText primary="Dashboard" />
         </ListItemButton>
+
         <ListItemButton>
             <ListItemIcon>
                 <ShoppingCartIcon />
             </ListItemIcon>
             <ListItemText primary="Coches" />
         </ListItemButton>
+
         <ListItemButton>
             <ListItemIcon>
                 <PeopleIcon />
             </ListItemIcon>
             <ListItemText primary="Reserva" />
         </ListItemButton>
+
         <ListItemButton>
             <ListItemIcon>
                 <PeopleIcon />
             </ListItemIcon>
             <ListItemText primary="Ubicacion" />
         </ListItemButton>
+
         <ListItemButton>
             <ListItemIcon>
                 <BarChartIcon />
             </ListItemIcon>
             <ListItemText primary="Facturacion" />
         </ListItemButton>
+
         <ListItemButton>
             <ListItemIcon>
                 <LayersIcon />
             </ListItemIcon>
             <ListItemText primary="Salir" />
         </ListItemButton>
+
     </React.Fragment>
 );
 
--- src/front/js/pages/userprofile.js
+++ src/front/js/pages/userprofile.js
@@ -22,6 +22,7 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import { mainListItems } from '../component/listitem';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import { ParkingView } from "../component/parkingview";
 
 function Copyright(props) {
   return (
@@ -84,34 +85,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 );
 
 const mdTheme = createTheme();
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""',
-    },
-  },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
-      opacity: 1,
-    },
-    '100%': {
-      transform: 'scale(2.4)',
-      opacity: 0,
-    },
-  },
-}));
+
 
 export const UserProfile = () => {
   const [open, setOpen] = React.useState(true);
@@ -199,15 +173,7 @@ export const UserProfile = () => {
           <Divider />
 
           <List component="nav"><br />
-            <Stack direction="row" spacing={2}>
-              <StyledBadge
-                overlap="circular"
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                variant="dot"
-              >
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-              </StyledBadge>
-            </Stack><br></br>
+            <br></br>
             {mainListItems}
           </List>
         </Drawer>
@@ -235,7 +201,7 @@ export const UserProfile = () => {
                     height: 240,
                   }}
                 >
-
+                  <ParkingView />
                 </Paper>
               </Grid>
 
