16d68796efd52c8493cd55b6a0c5bbb518c9aa98
danielo8417 <danieljuhe@gmail.com>
2023-03-15 19:30:29 +0000

    delete all

--- src/front/js/component/admin/users/adminuserlist.js
+++ src/front/js/component/admin/users/adminuserlist.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import { Button } from "@mui/material";
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 import { AdminUserModal } from "./adminusermodal";
+import swal from "sweetalert";
 
 export const AdminUserList = ({ user, roles, index, setUsers, setRoles }) => {
+
+
+    const mostrarAlerta = () => {
+        swal({
+            title: "App Parking",
+            text: "Plaza seleccionada con exito",
+            icon: "success",
+            button: "Aceptar",
+            timer: "9000"
+
+        })
+        navigate("/Privateuser");
+    }
+
+    const EraseUser = async () => {
+
+        const response = await fetch(process.env.BACKEND_URL + "/api/delete_users/" + user.id,
+            {
+                method: "DELETE",
+                headers: {
+                    "Content-type": "application/json",
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                },
+                body: JSON.stringify(user),
+            })
+        if (response.ok) {
+            setUsers((prevUsers) => prevUsers.filter((prevUser) => prevUser.id !== user.id));
+        }
+    };
+
     return (
         <TableRow
             key={index}
@@ -29,7 +60,7 @@ export const AdminUserList = ({ user, roles, index, setUsers, setRoles }) => {
                 </Button>
             </TableCell>
             <TableCell align="right">
-                <Button variant="contained" >
+                <Button variant="contained" onClick={EraseUser}>
                     <DeleteForeverOutlinedIcon />
                 </Button>
             </TableCell>
